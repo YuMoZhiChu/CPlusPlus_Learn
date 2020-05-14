12d0b5b3059c6a355bc5902abbf486435eff3bb1
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-05-14 22:32:16 +0800

    1

---
+++ src/prime_learn_code/05_14__const_ref_point_learn.cpp
@@ -0,0 +1,22 @@
+﻿// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+	int * p = new int(1);
+	const int *const q = p;
+	int* r = q;
+
+	int k = 10;
+	int & l = k;
+	const int & j = l;
+	int & m = j;
+
+	const int a = 1;
+	int b = a;
+
+	*p = 2;
+	std::cout << *q << std::endl;
+}
--- src/prime_learn_code/main.cpp
+++ src/prime_learn_code/main.cpp
@@ -1,9 +1,9 @@
-﻿#include<iostream>
-using namespace std;
-
-int main()
-{
-	std::cout << "hello world prime" << std::endl;
-
-	std::cout << 012 << std::endl;
-}
+﻿//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	std::cout << "hello world prime" << std::endl;
+//
+//	std::cout << 012 << std::endl;
+//}
