f6999bc35bd628061d0813a9c6159a5d2060b4ff
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-05-28 11:42:48 +0800

    1

--- src/prime_learn_code/05_14__const_ref_point_learn.cpp
+++ src/prime_learn_code/05_14__const_ref_point_learn.cpp
@@ -1,12 +1,8 @@
 ﻿#include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
-	int i;
-	char c = 'a';
-	char *pc = "hello";
-	i = int(*pc);
-	void *v;
-	v = pc;
-	char *v_c = static_cast<char*>(v);
+	int *ip = new int(65);
+	char *pc = reinterpret_cast<char*>(ip);
 }
