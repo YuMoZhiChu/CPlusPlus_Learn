32b2ac6cda32de8c768bea99f72e83a92310bfe5
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-06-29 21:15:14 +0800

    第九章 一小节 笔记
    1

--- src/prime_learn_code/main.cpp
+++ src/prime_learn_code/main.cpp
@@ -1,17 +1,32 @@
-﻿#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
+﻿
+#include <iostream>
+#include <vector>
+using std::vector;
+
+void insertDoubleValue(vector<int>& iv, int some_val)
+{
+	auto cursor = iv.size() / 2;
+	auto iter = iv.begin(), mid = iv.begin() + cursor;
+	while (iter != mid) {
+		if (*iter == some_val) {
+			iter = iv.insert(iter, 2 * some_val);
+			++iter;
+			++cursor;
+			mid = iv.begin() + cursor;
+		}
+		++iter;
+	}
+}
+
+void print(const vector<int>& iv)
+{
+	for (auto i : iv) std::cout << i << " ";
+	std::cout << std::endl;
+}
 
 int main()
 {
-	cin.tie(&cout);
-	int i;
-	cin >> i;
-	cout << i;
-	cin.tie();
-	int j;
-	cin >> j;
-	cout << j;
+	vector<int> iv = { 1, 1, 1, 1, 1, 7, 1, 9 };
+	insertDoubleValue(iv, 1);
+	print(iv);
 }
-// 这段程序会输出 1-10 的值
