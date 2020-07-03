c082c11687cafb766af4918543c02a6d5237968a
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-07-03 22:00:41 +0800

    第9章学习

--- src/prime_learn_code/main.cpp
+++ src/prime_learn_code/main.cpp
@@ -1,32 +1,105 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
-using std::vector;
+#include <cstdlib>
+#include <time.h>
+using namespace std;
 
-void insertDoubleValue(vector<int>& iv, int some_val)
-{
-	auto cursor = iv.size() / 2;
-	auto iter = iv.begin(), mid = iv.begin() + cursor;
-	while (iter != mid) {
-		if (*iter == some_val) {
-			iter = iv.insert(iter, 2 * some_val);
-			++iter;
-			++cursor;
-			mid = iv.begin() + cursor;
-		}
-		++iter;
+template <typename T>
+void Shuffle(T *samp, int count, int nDimensions) {
+	srand(time(NULL));
+	for (int i = 0; i < count; ++i) {
+		int other = i + rand() % (count - i);
+		for (int j = 0; j < nDimensions; ++j)
+			std::swap(samp[nDimensions * i + j], samp[nDimensions * other + j]);
 	}
 }
 
-void print(const vector<int>& iv)
-{
-	for (auto i : iv) std::cout << i << " ";
-	std::cout << std::endl;
-}
+// Point Declarations
+template <typename T>
+class Point2 {
+public:
+	// Point2 Public Methods
+	Point2() { x = y = 0; }
+	Point2(T xx, T yy) : x(xx), y(yy) {}
+
+	template <typename U>
+	explicit Point2(const Point2<U> &p) {
+		x = (T)p.x;
+		y = (T)p.y;
+	}
+
+
+	Point2(const Point2<T> &p) {
+		DCHECK(!p.HasNaNs());
+		x = p.x;
+		y = p.y;
+	}
+
+	Point2<T> &operator=(const Point2<T> &p) {
+		DCHECK(!p.HasNaNs());
+		x = p.x;
+		y = p.y;
+		return *this;
+	}
+	Point2<T> operator-() const { return Point2<T>(-x, -y); }
+	Point2<T> &operator+=(const Point2<T> &p) {
+		DCHECK(!p.HasNaNs());
+		x += p.x;
+		y += p.y;
+		return *this;
+	}
+	Point2<T> operator+(const Point2<T> &p) const {
+		DCHECK(!p.HasNaNs());
+		return Point2<T>(x + p.x, y + p.y);
+	}
+	template <typename U>
+	Point2<T> operator*(U f) const {
+		return Point2<T>(f * x, f * y);
+	}
+	template <typename U>
+	Point2<T> &operator*=(U f) {
+		x *= f;
+		y *= f;
+		return *this;
+	}
+	template <typename U>
+	Point2<T> operator/(U f) const {
+		CHECK_NE(f, 0);
+		Float inv = (Float)1 / f;
+		return Point2<T>(inv * x, inv * y);
+	}
+	template <typename U>
+	Point2<T> &operator/=(U f) {
+		CHECK_NE(f, 0);
+		Float inv = (Float)1 / f;
+		x *= inv;
+		y *= inv;
+		return *this;
+	}
+	T operator[](int i) const {
+		DCHECK(i >= 0 && i <= 1);
+		if (i == 0) return x;
+		return y;
+	}
+
+	T &operator[](int i) {
+		DCHECK(i >= 0 && i <= 1);
+		if (i == 0) return x;
+		return y;
+	}
+	bool operator==(const Point2<T> &p) const { return x == p.x && y == p.y; }
+	bool operator!=(const Point2<T> &p) const { return x != p.x || y != p.y; }
+
+	// Point2 Public Data
+	T x, y;
+};
+
 
 int main()
 {
-	vector<int> iv = { 1, 1, 1, 1, 1, 7, 1, 9 };
-	insertDoubleValue(iv, 1);
-	print(iv);
+	Point2<float> p1(1.0, 2.0);
+	Point2<float> p2(3.0, 4.0);
+
+	std::swap(p1.x, p2.x);
+
 }
