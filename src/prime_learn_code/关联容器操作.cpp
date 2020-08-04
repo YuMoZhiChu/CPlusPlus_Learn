f0e4843cf711a861200282d9cfb9ce9dd41eb425
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-08-04 22:38:06 +0800

    11 章笔记

---
+++ src/prime_learn_code/关联容器操作.cpp
@@ -0,0 +1,21 @@
+﻿//#include <iostream>
+//#include <map>
+//using namespace std;
+//
+//
+//int main(void)
+//{
+	//multimap<string, int> m;
+	//m = { {"Alan", 2},
+	//	 {"John", 3},
+	//	{"John", 3},
+	//	{"John2", 4} };
+	//
+	//auto it = m.lower_bound("John1"); // it 指向 John2 因为 John1 就是应该在 John 和 John2 之间
+//
+//	auto hasher = hash<string>();
+//	auto result = hash<string>() ("123");
+//	auto result2 = hasher("123");
+//
+//	return 0;
+//}
---
+++ src/prime_learn_code/动态指针学习.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <map>
+#include <memory>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	vector<string> b1;
+	{
+		vector<string> b2 = { "a1", "b2" };
+		b1 = b2;
+	}
+
+	return 0;
+}
