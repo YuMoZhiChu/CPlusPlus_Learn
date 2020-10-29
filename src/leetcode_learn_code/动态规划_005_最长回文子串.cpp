9801f6a82ba1e794b80bffcade4466bb19a165ea
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-10-30 01:21:50 +0800

    11

---
+++ src/leetcode_learn_code/动态规划_005_最长回文子串.cpp
@@ -0,0 +1,57 @@
+﻿#include<vector>
+using std::vector;
+
+#include<string>
+using std::string;
+
+class Solution {
+public:
+	// 动归做法
+	string longestPalindrome(string s) {
+		if (s == "") return "";
+		// 结果取一个大的即可
+		const int size = s.size();
+		// dp 的意义, 是以第i个词结尾, 存在的 可能的回文子序列 的长度
+		vector<vector<int>> dp;
+		// 初始化, 每个变量都是 1 的长度
+		for (int i = 0; i < size; ++i)
+		{
+			dp.push_back({ 1 });
+		}
+
+		for (int i = 1; i < size; ++i)
+		{
+			// 从 i 开始, 做假设遍历
+			if (s[i] == s[i - 1])
+			{
+				dp[i].push_back(2);
+			}
+			// 如果和对称的一个相等
+			for (int parlin_leght : dp[i - 1])
+			{
+				if (i - 1 - parlin_leght >= 0 && s[i] == s[i - 1 - parlin_leght])
+				{
+					dp[i].push_back(parlin_leght + 2);
+				}
+			}
+		}
+
+		// TODO 优化, 可以把这个遍历的过程, 直接放在上面的比较流程中
+		// 遍历, 取最大值
+		int result = 0;
+		int pos = 0;
+		for (int i = 0; i < size; ++i)
+		{
+			for (int& parlin_leght : dp[i])
+			{
+				if (parlin_leght > result)
+				{
+					result = parlin_leght;
+					pos = i;
+				}
+			}
+		}
+
+		return s.substr(pos - result + 1, result);
+	}
+};
---
+++ src/leetcode_learn_code/动态规划_005_最长回文子串_马拉车方法.cpp
@@ -0,0 +1,59 @@
+﻿#include<string>
+using std::string;
+
+class Solution {
+public:
+	// 获得以该位置为中心的半径
+	// 这里如果用 const string S, stirng S, 内存会爆炸, 使用引用来优化内存，const string &s 能被编译器优化，不需要新建一个 temp string
+	int getRadius(const string &S, const int &pos)
+	{
+		int result = 0;
+		int left = pos - 1;
+		int right = pos + 1;
+		while (left >= 0 && right < S.size())
+		{
+			if (S[left] == S[right])
+			{
+				++result;
+				--left;
+				++right;
+			}
+			else {
+				break;
+			}
+
+		}
+		return result;
+	}
+
+	// 马拉车做法
+	string longestPalindrome(string s) {
+		if (s == "") return "";
+
+		// 预处理字符串, 间隔都加一个 #
+		string S = "#";
+
+		for (char c : s)
+		{
+			S += c;
+			S += '#';
+		}
+
+		// 马拉车, 计算辅助数组, 或者是 回文半径数组
+		const int size = S.size();
+		int *mrh = new int[size]();
+		int max_radius = 1;
+		int start_pos = 0;
+		for (int i = 0; i < size; ++i)
+		{
+			int cur_radius = getRadius(S, i);
+			if (cur_radius > max_radius)
+			{
+				max_radius = cur_radius;
+				start_pos = (i - max_radius) / 2;
+			}
+		}
+		// 辅助数组中的最大半径, 就是结果
+		return s.substr(start_pos, max_radius);
+	}
+};
