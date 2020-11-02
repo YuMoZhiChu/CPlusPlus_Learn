23001c57e991386f4e53a838427cc7a299d33a08
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-11-02 20:02:50 +0800

    1

---
+++ src/leetcode_learn_code/动态规划_007_编辑距离.cpp
@@ -0,0 +1,59 @@
+﻿#include<string>
+using std::string;
+
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		const int word1_size = word1.size();
+		const int word2_size = word2.size();
+		// dp 的意义, dp[i][j] 表示 word1[i](表示长度为i的word1子串) 变成 word2[j] 的需要的最少操作数
+		// 
+		int **dp = new int*[word1_size + 1](); // 初始为 0
+		for (int i = 0; i <= word1_size; ++i)
+		{
+			dp[i] = new int[word2_size + 1]();
+		}
+
+		// base 基础情况, 当一条word是0长度时, 需要的操作数, 就是另一条的长度
+		for (int i = 0; i <= word1_size; ++i)
+			dp[i][0] = i;
+		for (int j = 0; j <= word2_size; ++j)
+			dp[0][j] = j;
+
+		// 动归递归
+		// dp[i][j]
+		// if w1[i] == w2[j] -> dp[i-1][j-1]
+		// if w1[i] != w2[j]
+		//  min{
+		//  dp[i][j-1] + 1 // 插入
+		//  dp[i-1][j] + 1 // 删除
+		//  dp[i-1][j-1] + 1 // 替换  
+		// }
+		for (int i = 1; i <= word1_size; ++i)
+		{
+			for (int j = 1; j <= word2_size; ++j)
+			{
+				if (word1[i - 1] == word2[j - 1])
+				{
+					dp[i][j] = dp[i - 1][j - 1];
+				}
+				else {
+					dp[i][j] = dp[i][j - 1] + 1;
+					if (dp[i - 1][j] + 1 < dp[i][j])
+						dp[i][j] = dp[i - 1][j] + 1;
+					if (dp[i - 1][j - 1] + 1 < dp[i][j])
+						dp[i][j] = dp[i - 1][j - 1] + 1;
+				}
+			}
+		}
+
+		// 获取结果
+		int result = dp[word1_size][word2_size];
+		for (int i = 0; i <= word1_size; ++i)
+		{
+			delete[] dp[i];
+		}
+		delete[] dp;
+		return result;
+	}
+};
---
+++ src/leetcode_learn_code/动态规划_008_单词拆分.cpp
@@ -0,0 +1,85 @@
+﻿#include<string>
+using std::string;
+
+#include<queue>
+using std::queue;
+
+#include<unordered_map>
+using std::unordered_map;
+
+#include<unordered_set>
+using std::unordered_set;
+
+#include<vector>
+using std::vector;
+
+struct ResultNode {
+	ResultNode(string _answer, string _nokori) :answer(_answer), nokori(_nokori) {}
+	string answer;
+	string nokori;
+};
+
+// 这道题, 是 动归 + 记录本 的递归写法, 如果使用广搜, 会被重复节点耗死
+// 标准答案, 是从顶向下 的东西
+
+class Solution {
+public:
+	vector<string> wordBreak(string s, vector<string>& wordDict) {
+		// 做特殊处理
+		for (char &c : s)
+		{
+			bool is_in = false;
+			for (string &word : wordDict)
+			{
+				if (word.find(c) != -1)
+				{
+					is_in = true;
+					break;
+				}
+			}
+			if (!is_in)
+				return vector<string>();
+		}
+
+
+		// 自底向上的解法 -> 缺点, 不能快速的过度到不符合预期的特例, 会导致展开过大, -> 失败
+		// dp 的含义 s 的对应解法 []
+		unordered_map<string, vector<string>> dp;
+		// base 情况
+		dp[""] = vector<string>(1, "");
+
+		for (int i = 0; i < s.size(); ++i)
+		{
+			// 这里 的 s, 是长度大小, 所以要 i + 1
+			dp[s.substr(0, i + 1)] = vector<string>();
+			for (string &word : wordDict)
+			{
+				// base 小于 word 大小的, 都是空
+				if ((i + 1) < word.size())
+					continue;
+				if (s.substr((i + 1) - word.size(), word.size()) == word)
+				{
+					string head_string = s.substr(0, (i + 1) - word.size());
+					if (dp.count(head_string))
+					{
+						for (string &answer : dp[head_string])
+						{
+							// 像这种情况, 确实存在一定会超时的情况
+							// 就算是使用深搜, 虽然能过 aaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaa 的测试样例
+							// 但是如果样例改成 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+							// 就也一样过不了, 个人认为, 这道题用深搜能解决的原因是
+							// 深搜能快速过度到我们 aaaaaaaaaaaabaaaaaaaaaaa 中不符合预期的情况, 并且把整个逻辑链给断了
+							if (answer != "")
+								dp[s.substr(0, i + 1)].push_back(answer + " " + word);
+							else
+								dp[s.substr(0, i + 1)].push_back(word);
+						}
+					}
+				}
+			}
+		}
+
+		// 返回最终结果
+		return dp[s];
+	}
+};
