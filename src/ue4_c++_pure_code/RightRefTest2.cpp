c65065ad05e3f4450149025c30a6cb0507dd35c0
ikun <695342954@qq.com>
2023-01-05 15:39:12 +0800

    0105 学习
    学习 UE4 的一些常用到的内存偏移和处理的方法

--- src/ue4_c++_pure_code/RightRefTest2.cpp
+++ src/ue4_c++_pure_code/RightRefTest2.cpp
@@ -1,74 +1,74 @@
-﻿#include <iostream>
-#include <string.h>
-
-using namespace std;
-
-class A
-{
-public:
-
-	A(int InA = 1)
-	{
-		a = InA;
-	}
-
-	int* GetAPtr()
-	{
-		return &a;
-	}
-
-private:
-
-	int a = 1;
-
-};
-
-
-A BuildAObject()
-{
-	return A(1);
-}
-
-
-
-__forceinline bool TestAnd()
-{
-	return true || true;
-}
-
-void ChangeTest(int&& right_value)
-{
-	static int TestCount = 0;
-	++TestCount;
-	right_value = TestCount;
-}
-
-int main()
-{
-	int* x = nullptr;
-	{
-		x = BuildAObject().GetAPtr();
-	}
-
-	int bk = 1;
-	++bk;
-
-	/*while (true)
-	{
-		++bk;
-	}*/
-	
-	bool testResult = !TestAnd();
-
-
-	int a = 999;
-	int& a_ref = a;
-	int&& a_right_ref = std::move(a);
-	ChangeTest(std::move(a));
-	ChangeTest(std::move(a_ref));
-	ChangeTest(std::move(a_right_ref));
-
-
-	++bk;
-
-}
+﻿//#include <iostream>
+//#include <string.h>
+//
+//using namespace std;
+//
+//class A
+//{
+//public:
+//
+//	A(int InA = 1)
+//	{
+//		a = InA;
+//	}
+//
+//	int* GetAPtr()
+//	{
+//		return &a;
+//	}
+//
+//private:
+//
+//	int a = 1;
+//
+//};
+//
+//
+//A BuildAObject()
+//{
+//	return A(1);
+//}
+//
+//
+//
+//__forceinline bool TestAnd()
+//{
+//	return true || true;
+//}
+//
+//void ChangeTest(int&& right_value)
+//{
+//	static int TestCount = 0;
+//	++TestCount;
+//	right_value = TestCount;
+//}
+//
+//int main()
+//{
+//	int* x = nullptr;
+//	{
+//		x = BuildAObject().GetAPtr();
+//	}
+//
+//	int bk = 1;
+//	++bk;
+//
+//	/*while (true)
+//	{
+//		++bk;
+//	}*/
+//	
+//	bool testResult = !TestAnd();
+//
+//
+//	int a = 999;
+//	int& a_ref = a;
+//	int&& a_right_ref = std::move(a);
+//	ChangeTest(std::move(a));
+//	ChangeTest(std::move(a_ref));
+//	ChangeTest(std::move(a_right_ref));
+//
+//
+//	++bk;
+//
+//}
